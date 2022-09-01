2eb86d3365fbda3f3ead2271f984fd84d670ac91
Luis Hernández Gómez <ing.luishg76@gmail.com>
2022-09-01 08:03:48 -0500

    salva(01-09-022)-laptop

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,3 +2,4 @@ export 'package:peliculas_app/models/movie.dart';
 export 'package:peliculas_app/models/now_playing.dart';
 export 'package:peliculas_app/models/popular.dart';
 export 'package:peliculas_app/models/movie_credits.dart';
+export 'package:peliculas_app/models/movies_found.dart';
